72786cc0e2e3db4e456ae49530f550fd9440791a
Chai Wei Wong <wongcwei@gmail.com>
2026-08-22 18:43:07 +1200

    placeholder screen for main

--- app/src/main/java/com/jobapplicationapp/jobby/MainActivity.kt
+++ app/src/main/java/com/jobapplicationapp/jobby/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.jobapplicationapp.jobby.ui.JobApplicationListScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,5 +28,5 @@ class MainActivity : ComponentActivity() {
  */
 @Composable
 fun JobbyApp(modifier: Modifier = Modifier) {
-
+    JobApplicationListScreen(modifier = modifier)
 }
