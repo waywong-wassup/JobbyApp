70ef3c0be8685dea4333e2cd59ef1b7b963d786a
Chai Wei Wong <wongcwei@gmail.com>
2026-08-28 23:03:09 +1200

    setup fake repository for unit tests. Add ViewModel unit tests

--- app/src/main/java/com/jobapplicationapp/jobby/ui/JobApplicationViewModel.kt
+++ app/src/main/java/com/jobapplicationapp/jobby/ui/JobApplicationViewModel.kt
@@ -56,15 +56,20 @@ class JobApplicationViewModel (
 
     fun saveJobApplicationChange() {
         val changes = _changingJobApplication.value
-        if (changes != null) {
+        if (changes != null && validateInput() == ValidationError.NONE) {
             viewModelScope.launch {
-                if (changes.jobApplicationId == 0) {
-                    jobApplicationRepository.addJobApplication(changes)
-                } else {
-                    jobApplicationRepository.updateJobApplication(changes)
+                try {
+                    if (changes.jobApplicationId == 0) {
+                        addJobApplication(changes)
+                    } else {
+                        updateJobApplication(changes)
+                    }
+                } catch (e: Exception) {
+                    println("Error saving job application: ${e.message}")
                 }
             }
         }
+
     }
     fun selectJob(job: JobApplication) {
         _changingJobApplication.value = job
