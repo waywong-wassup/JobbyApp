2d9f728e1e3b0abe320595e13d37a017f3549866
Chai Wei Wong <wongcwei@gmail.com>
2026-09-03 14:56:17 +1200

    fix naming for progress

--- app/src/main/java/com/jobapplicationapp/jobby/data/JobApplication.kt
+++ app/src/main/java/com/jobapplicationapp/jobby/data/JobApplication.kt
@@ -36,7 +36,7 @@ enum class Progress (val progressPhase: String) {
     APPLIED("Applied"),
     SCREENCALL("Screen Call"),
     INTERVIEW("Interview"),
-    OFFER("Offered"),
+    OFFERED("Offered"),
     REJECTED("Rejected"),
     ACCEPTED("Accepted")
 }
